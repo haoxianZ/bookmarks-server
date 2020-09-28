3477660c11d8c197e0a3392f23c23efded6d63a8
John Zhang <johnhaoxian@gmail.com>
2020-09-28 16:56:14 -0400

    hidding postgres

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks-test',
-  DATABASE_URL: process.env.DATABASE_URL ||'postgres://quutzmxvpivgqu:22e56894ed12fc00d5937139c3a5fc186453a52d984d9b3a07f588b2818b886a@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d831nl1i0c6i3b'
+  DATABASE_URL: process.env.DATABASE_URL ||'postgresql://dunder_mifflin@localhost/bookmarks'
 }
