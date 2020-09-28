ae346040fcc1dafbfe8bf7a4ec10359784a40be8
John Zhang <johnhaoxian@gmail.com>
2020-09-28 09:56:43 -0400

    pre deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,6 +5,6 @@ module.exports = {
     "driver": "pg",
     'validateChecksums': false,
   "connectionString": (process.env.NODE_ENV === 'test')
-     ? process.env.TEST_DB_URL
-     : process.env.DB_URL,
+     ? process.env.TEST_DataBase_URL
+     : process.env.DataBase_URL,
   }
