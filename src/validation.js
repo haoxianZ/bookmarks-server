170c726ad0478d5c2b30ec5c7d99bd92cd554b03
John Zhang <johnhaoxian@gmail.com>
2020-09-28 23:05:03 -0400

    changing 401 message

--- src/validation.js
+++ src/validation.js
@@ -6,7 +6,7 @@ function validation(req, res, next) {
 
     if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
       logger.error(`Unauthorized request to path: ${req.path}`)
-      return res.status(401).json({ error: 'Unauthorized request' })
+      return res.status(401).json({ error: 'Unauthorized request from the server' })
     }
     // move to the next middleware
     next()
